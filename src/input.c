cdc46a6664170cd23d84e87c3cae6038bfe311dc
Jake Vasilakes <jvasilakes@gmail.com>
2014-11-10 16:14:14 -0600

    Implemented parse_cmd().

--- src/input.c
+++ src/input.c
@@ -1,71 +1,108 @@
 #include "input.h"
 
 
-void put_input(int c)
+void put_input(struct GapBuffer *buf, int c)
 {
     /**********************************
      * Print character to the screen  *
      * taking special characters into *
      * account as necessary.          *
      **********************************/
+
     int y, x;
+    getyx(stdscr, y, x);
 
     switch(c)
     {
-        // 127: Backspace 8: Delete
-        case 127:
-        case 8:
-            getyx(stdscr, y, x);
-            mvdelch(y, x-1);
-            refresh();
-            break;
-        // 13: Enter/Return
-        case 13:
-            getyx(stdscr, y, x);
-            move(y+1, x-x);
-            refresh();
-            break;
         // 27: Esc - Do nothing.
         case 27:
             break;
+
+        case KEY_BACKSPACE:
+            // Delete from the buffer.
+            GBdelchar(buf);
+            // Delete from the screen.
+            mvdelch(y, x-1);
+            wnoutrefresh(stdscr);
+            break;
+
+        case KEY_RIGHT:
+            GBstepForward(buf);
+            move(y, x+1);
+            wnoutrefresh(stdscr);
+            break;
+
+        case KEY_LEFT:
+            GBstepBackward(buf);
+            move(y, x-1);
+            wnoutrefresh(stdscr);
+            break;
+
         default:
+            // Add to the buffer.
+            GBaddchar(buf, c);
+            // Add to the screen.
             waddch(stdscr, c);
-            refresh();
+            wnoutrefresh(stdscr);
     }
+}
+
+
+int parse_cmd(struct GapBuffer *buf, int c)
+{
+    if (c == 'w') {
+        GBwrite(buf);
+        return 1;
+    }
+
+    return 0;
 }
 
 
 void input_mgmt()
 {
-/******************
- *     state      *
- *  0 - edit mode *
- *  1 - cmd mode  *
- * -1 - quit      *
- ******************/
+    /******************
+     *     state      *
+     *  0 : edit mode *
+     *  1 : cmd mode  *
+     * -1 : quit      *
+     ******************/
     int y_size, x_size;     // Main window dimensions.
-    WINDOW *state_win;
     int state = 0;
     int c;
 
+    // Window to display the current state.
+    WINDOW *state_win;
     getmaxyx(curscr, y_size, x_size);
     state_win = newwin(1, x_size, y_size-1, 0);
 
-    display_state(state_win, state);
+    // Text buffer.
+    struct GapBuffer *buf;
+    buf = GBinit();
+
+    // Needed for immediate escape key behavior.
+    if (getenv("ESCDELAY") == NULL) {
+        ESCDELAY = 25;
+    }
+
+
     while (state != -1) {
-        c = getchar();
+        c = getch();
         state = get_state(c, state);
         display_state(state_win, state);
 
         if (state == 0) {
-            put_input(c);
+            put_input(buf, c);
         }
         else if (state == 1) {
-            //parse_cmd(c);
-            ;
+            if ( parse_cmd(buf, c) ) {
+                wprintw(state_win, " written");
+                wnoutrefresh(state_win);
+            }
         }
         else {
             break;
         }
+        doupdate();
     }
 }
--- src/input.h
+++ src/input.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 #include "state.h"
+#include "buffer.h"
 
-void put_input(int);
+void put_input(struct GapBuffer *buf, int);
+int parse_cmd(struct GapBuffer *buf, int);
 void input_mgmt();
